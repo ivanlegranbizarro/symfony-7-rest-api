bcdd6e08e62b2b3d8721b13fc9af34d2cfe2998b
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-22 00:03:35 +0200

    validaciones

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -6,8 +6,11 @@ use App\Repository\ProjectRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: ProjectRepository::class)]
+#[UniqueEntity('name', message: 'The name must be unique')]
 class Project
 {
   #[ORM\Id]
@@ -17,10 +20,15 @@ class Project
   private ?int $id = null;
 
   #[ORM\Column(length: 255)]
+  #[Assert\NotBlank]
+  #[Assert\Length(min: 3, max: 100)]
   #[Groups(['project:read'])]
   private ?string $name = null;
 
   #[ORM\Column(type: Types::TEXT)]
+  #[Assert\NotBlank]
+  #[Assert\Length(min: 3, minMessage: 'The description must be at least 3 characters long')]
+  #[Assert\Length(max: 1000, maxMessage: 'The description must be at most 1000 characters long')]
   #[Groups(['project:read'])]
   private ?string $description = null;
 
